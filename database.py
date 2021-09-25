c0d90fcbba3704cd6edff90b4a147d18d22d5bc9
duranddurand <duran.m@icloud.com>
2021-09-25 15:54:25 +0100

    Class database methode player_table

--- database.py
+++ database.py
@@ -1,3 +1,11 @@
 from tinydb import TinyDB
 
-database = TinyDB('database.json')
+class Database:
+    def __init__(self):
+        self.db = 'db.json'
+
+    def player_table(self):
+        db = TinyDB(self.db)
+        table = db.table('players')
+
+        return table
